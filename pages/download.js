74e194833893f5b4df9b5ccecdded2c206da7675
Gianmarco <cavallogianmarco@gmail.com>
2020-03-31 19:00:16 +0200

    initial setup

---
+++ pages/download.js
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const Download = () => <div className="container">PIPPO</div>;
+
+export default Download;
--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,168 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import theme from "./theme";
+import { Button } from "antd";
 
 const Home = () => (
   <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    {/* <main>
+  <div className="Container">
+</main> */}
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
+    <div className="gradient">
+      <div className="navBar">
+        <img alt="logo" src="gdLogo.png" className="logo" />
+        {/* <Button>Home</Button> */}
+        <div className="buttons">
+          <div className="button" id="home">
+            Home
+          </div>
+          <Link href="/about">
+            <div className="button">About</div>
+          </Link>
+          <Link href="/download">
+            <div id="download" className="button">
+              Download
+            </div>
+          </Link>
+        </div>
       </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+      <div className="homePageTitleContainer">
+        <img alt="logo" src="gdLogo.png" className="logoBig" />
+        <div className="homePageTitle">
+          <h1>GDLauncher</h1>
+          <p>The future of minecraft launchers</p>
+          <button>Download</button>
+        </div>
+      </div>
+      <img alt="isometric" src="isometricLauncher.png" className="isometric" />
+    </div>
 
     <style jsx>{`
       .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
+        height: 400vh;
+        width: 100%;
         display: flex;
         flex-direction: column;
         justify-content: center;
+        background: #2f5266;
+      }
+
+      .gradient {
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        left: 0;
+        top: 0;
+        display: flex;
+        flex-direction: column;
         align-items: center;
+        background: linear-gradient(
+          180deg,
+          #212b36 50.77%,
+          rgba(33, 43, 54, 0) 100%
+        );
+      }
+
+      .homePageTitleContainer {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 1098px;
+        height: 450px;
+        z-index: 2;
+        position: absolute;
+        top: 200px;
+      }
+
+      .homePageTitle {
+        width: 589px;
+        height: 360px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: flex-start;
+        color: white;
+      }
+
+      .homePageTitle > h1 {
+        width: 100%;
+        color: white;
+
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: 900;
+        font-size: 72px;
+        line-height: 84px;
+      }
+
+      .homePageTitle > p {
+        color: white;
+
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 48px;
+        line-height: 56px;
+      }
+
+      .logoBig {
+        width: 430px;
+
+        z-index: 1;
+      }
+
+      .isometric {
+        position: absolute;
+        max-width: 100%;
+        height: auto;
+        top: 80px;
+        z-index: 0;
+      }
+
+      .navBar {
+        width: 100%;
+        height: 80px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      #download {
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: 900;
+        font-size: 20px;
+        line-height: 28px;
+        color: #27ae60;
+      }
+
+      .logo {
+        width: 54px;
+        margin: 0 0 0 24px;
+      }
+
+      .button {
+        margin: 10px;
+      }
+
+      .buttons {
+        width: 400px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        color: white;
+      }
+
+      .buttons > div {
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 28px;
       }
 
       main {
@@ -74,113 +172,6 @@ const Home = () => (
         flex-direction: column;
         justify-content: center;
         align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
       }
     `}</style>
 
@@ -198,6 +189,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
