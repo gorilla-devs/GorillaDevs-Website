d304817004f2f5404e7ecd63e9e134ac7c0cfec0
Davide <davide.ceschia@gmail.com>
2020-04-02 19:56:49 +0200

    Fixed production

--- next.config.js
+++ next.config.js
@@ -1,21 +1,45 @@
-module.exports = () => {
-  /* eslint-disable */
-  const withLess = require("@zeit/next-less");
-  const lessToJS = require("less-vars-to-js");
-  const fs = require("fs");
-  const path = require("path");
-  // Where your antd-custom.less file lives
-  const themeVariables = lessToJS(
-    fs.readFileSync(path.resolve(__dirname, "antd-custom.less"), "utf8")
-  );
-  // fix: prevents error when .less files are required by node
-  if (typeof require !== "undefined") {
-    require.extensions[".less"] = file => {};
-  }
-  return withLess({
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const path = require("path");
+const withLess = require("@zeit/next-less");
+const withCSS = require("@zeit/next-css");
+const withPlugins = require("next-compose-plugins");
+
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, "./antd-custom.less"), "utf8")
+);
+
+const plugins = [
+  withCSS,
+  withLess({
     lessLoaderOptions: {
       javascriptEnabled: true,
-      modifyVars: themeVariables // make your antd custom effective
+      modifyVars: themeVariables
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style.*?/;
+        const origExternals = [...config.externals];
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback();
+            if (typeof origExternals[0] === "function") {
+              origExternals[0](context, request, callback);
+            } else {
+              callback();
+            }
+          },
+          ...(typeof origExternals[0] === "function" ? [] : origExternals)
+        ];
+
+        config.module.rules.unshift({
+          test: antStyles,
+          use: "null-loader"
+        });
+      }
+      return config;
     }
-  });
-};
+  })
+];
+module.exports = withPlugins(plugins);
