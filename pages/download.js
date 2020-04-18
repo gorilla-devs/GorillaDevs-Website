22204847f8f1e2de8849a34e3e16fe474bc0bef1
Gianmarco <cavallogianmarco@gmail.com>
2020-04-18 18:13:46 +0200

    Revert "onePage website"
    This reverts commit eec3a0a91c17eb7f15773eaee9ba0f1866efd149.

---
+++ pages/download.js
@@ -0,0 +1,285 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinux,
+  faWindows,
+  faApple,
+} from "@fortawesome/free-brands-svg-icons";
+import theme from "../theme";
+import NavBar from "../components/navbar";
+
+const downloadLauncherBasedOnOs = (os) => {
+  console.log("os", os);
+  if (os === "Win32" || os === "Win16") {
+    console.log("OK");
+    return (
+      <Button
+        type="primary"
+        className="downloadButton"
+        style={{ display: "flex" }}
+        href="https://github.com/gorilla-devs/GDLauncher-Releases/releases/latest/download/GDLauncher-win32-setup.exe"
+      >
+        Download for Windows
+      </Button>
+    );
+  } else if (
+    os === "Darwin" ||
+    os === "Mac68K" ||
+    os === "MacPPC" ||
+    os === "MacIntel"
+  ) {
+    return (
+      <Button
+        type="primary"
+        className="downloadButton"
+        href="https://github.com/gorilla-devs/GDLauncher-Releases/releases/latest/download/GDLauncher-darwin-setup.dmg"
+        style={{ display: "flex" }}
+      >
+        Download for MacOs
+      </Button>
+    );
+  } else if (os === "linux" || os === "Linux i686" || os === "Linux armv7l") {
+    return (
+      <Button
+        type="primary"
+        className="downloadButton"
+        href="https://github.com/gorilla-devs/GDLauncher-Releases/releases/latest/download/GDLauncher-linux-setup.AppImage"
+        style={{ display: "flex" }}
+      >
+        Download for Linux
+      </Button>
+    );
+  }
+};
+
+const Download = () => {
+  const [os, setOs] = useState(null);
+
+  useEffect(() => {
+    if (window.navigator.platform) setOs(window.navigator.platform);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="navBar">
+        <NavBar />
+      </div>
+      <img
+        alt="launcher"
+        src="https://cdn.assets-gdevs.com/launcher-1.jpg"
+        className="launcher1"
+      />
+      <div className="gradient">
+        <h1 className="title">Download the launcher and have fun!</h1>
+        <div className="downloadContainer">
+          {os ? (
+            downloadLauncherBasedOnOs(os)
+          ) : (
+            <Button type="primary" className="downloadButton">
+              Download
+            </Button>
+          )}
+          <div className="icons">
+            <p>Available for:</p>
+            <FontAwesomeIcon
+              onClick={() => setOs("Win32")}
+              icon={faWindows}
+              size="2x"
+              style={{ color: theme.palette.text.primary, cursor: "pointer" }}
+            />
+            <FontAwesomeIcon
+              onClick={() => setOs("Darwin")}
+              icon={faApple}
+              size="2x"
+              style={{ color: theme.palette.text.primary, cursor: "pointer" }}
+            />
+            <FontAwesomeIcon
+              onClick={() => setOs("linux")}
+              icon={faLinux}
+              size="2x"
+              style={{ color: theme.palette.text.primary, cursor: "pointer" }}
+            />
+          </div>
+          <div className="portableContainer">
+            <a href="https://github.com/gorilla-devs/GDLauncher-Releases/releases/latest/download/GDLauncher-win32-portable.zip">
+              PortableWin
+            </a>
+
+            <a href="https://github.com/gorilla-devs/GDLauncher-Releases/releases/latest/download/GDLauncher-linux-portable.zip">
+              PortableLinux
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background: ${theme.palette.primary.dark};
+        }
+
+        .title {
+          color: ${theme.palette.text.primary};
+          position: absolute;
+          top: 20%;
+          font-weight: 900;
+          animation: fadeInLeft ease 2s;
+        }
+
+        .navBar {
+          position: absolute;
+          top: 0;
+          z-index: 2;
+          width: 100%;
+          height: 80px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+        }
+
+        .gradient {
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          left: 0;
+          top: 0;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          background: linear-gradient(
+            180deg,
+            #212b36 50.77%,
+            rgba(33, 43, 54, 0) 100%
+          );
+          justify-content: center;
+        }
+
+        #download {
+          font-style: normal;
+          font-weight: 900;
+          font-size: 20px;
+          line-height: 28px;
+          color: #27ae60;
+        }
+
+        .logo {
+          width: 54px;
+          margin: 0 0 0 24px;
+        }
+
+        @keyframes fadeInTop {
+          0% {
+            opacity: 0;
+            transform: translateY(-20px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+
+        @keyframes fadeInLeft {
+          0% {
+            opacity: 0;
+            transform: translateX(-20px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateX(0);
+          }
+        }
+
+        @keyframes MoveUpDown {
+          0% {
+            transform: translateY(0);
+          }
+          50% {
+            transform: translateY(-20px);
+          }
+          100% {
+            transform: translateY(0px);
+          }
+        }
+
+        .launcher1 {
+          animation: MoveUpDown 2s linear infinite, fadeInTop ease 2s;
+          max-width: 95%;
+          height: auto;
+          z-index: 0;
+        }
+
+        .button {
+          margin: 10px;
+        }
+
+        .portableButton {
+          background: transparent;
+        }
+
+        .buttons {
+          width: 400px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-around;
+          align-items: center;
+          color: ${theme.palette.text.primary};
+        }
+
+        .buttons > div {
+          font-style: normal;
+          font-weight: normal;
+          font-size: 16px;
+          line-height: 28px;
+        }
+
+        .downloadContainer {
+          position: absolute;
+          top: 30%;
+          z-index: 2;
+          display: flex;
+          flex-direction: column;
+          color: ${theme.palette.text.primary};
+          animation: fadeInLeft ease 1s;
+        }
+
+        .downloadContainer > .icons {
+          margin-top: 30px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+
+        .downloadContainer > p {
+          font-weight: 700;
+          width: 90px;
+        }
+
+        .portableContainer {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          margin: 0;
+        }
+
+        @media only screen and (max-height: 650px) {
+          .container {
+            height: 130vh;
+          }
+          .launcher1 {
+            max-width: 90%;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Download;
